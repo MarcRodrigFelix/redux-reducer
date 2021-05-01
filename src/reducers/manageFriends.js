5e9dab7b9ebaa3b26ceedc3740513489f0f72994
marcos <m.rodrig.felix@gmail.com>
2021-04-30 21:04:48 -0700

    commit tree 7a7abb5741d38ccf1c3c7ad4c2149df00cec34f2

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
-export function manageFriends(state, action){
+export function manageFriends(state = [], action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return ({
+                ...state,
+                friends: [
+                    ...state.friends,
+                    action.friend
+                ]
+            })
+        case 'REMOVE_FRIEND':
+            const removeFriendId = state.friends.findIndex( friend => friend.id === action.id )
+            return (
+                {...state,
+                friends: [
+                    ...state.friends.slice(0, removeFriendId),
+                    ...state.friends.slice(removeFriendId + 1)
+                ]}
+            )
+        default:
+            return state;
+    }
 }
